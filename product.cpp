be9ad4c741a788b1a43c44b667564ec8e200f481
Naveen2224 <65078185+Naveen2224@users.noreply.github.com>
2020-07-06 08:19:25 -0700

    created product.cpp
    created by naveen.We get the product of two numbers by this program

---
+++ product.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y;
+	cout<<"enter two numbers:";
+	cin>>x;
+	cin>>y;
+	cout<<"product="<<(x*y);
+	return 0;
+}
+
+
+
+
+#OUTPUT of this program:
+enter two numbers: 3.4          //user input//
+5.5
+product=18.7
